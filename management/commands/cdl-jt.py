85a552756a9b79509f3b6e967966a74acbe8c6e2
Tim Frazee <tim@castironcoding.com>
2022-02-01 21:10:33 -0800

    [F] Add install and cli scripts

--- management/commands/cdl-jt.py
+++ management/commands/cdl-jt.py
@@ -7,9 +7,22 @@ class Command(BaseCommand):
     help = 'Import some shit'
 
     def add_arguments(self, parser):
-        parser.add_argument("json_file", nargs=1, type=str)
+        parser.add_argument("operation", nargs=1, type=str)
+        parser.add_argument("--file", nargs=1, type=str)
+        parser.add_argument("--")
 
     def handle(self, **options):
+        if options["operation"] == "get":
+            self.handle_get(*args, **options)
+        elif options["operation"] == "put":
+            self.handle_put(*args, **options)
+
+
+    def handle_get(self, *args, **options):
+        pass # WIP
+
+
+    def handle_put(self, *args, **options):
         with open(options["json_file"][0]) as file:
             data = json.load(file)
 
